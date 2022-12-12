ca0ee6d7b349d4cd1ba9654839bd0c425f86c105
chiefmikey <wolfemikl@gmail.com>
2022-12-11 21:16:34 -0700

    Init

---
+++ configs/prettier-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  proseWrap: 'always',
+  svelteSortOrder: 'options-scripts-markup-styles',
+  svelteStrictMode: true,
+  svelteBracketNewLine: true,
+  svelteAllowShorthand: false,
+  svelteIndentScriptAndStyle: false,
+  vueIndentScriptAndStyle: false,
+};
