439a30504450eab57cad44052e59f85a208a03cf
Mikl Wolfe <wolfemikl@gmail.com>
2024-02-20 21:21:19 -0700

    feat(config): init eslint react

---
+++ eslint-config-react/README.md
@@ -0,0 +1,31 @@
+<div width="100%" align="center">
+  <h1>
+    <a href="https://github.com/chiefmikey/mikey-pro">
+      <b>Mikey Pro</b>
+    </a>
+  </h1>
+</div>
+
+## **@mikey-pro/eslint-config-react**
+
+A preset ESLint React configuration
+
+## Usage
+
+### Install
+
+```shell
+npm i -D mikey-pro @mikey-pro/eslint-config-react
+```
+
+### Configure
+
+Extend to ESLint in `package.json`:
+
+```json
+{
+  "eslintConfig": {
+    "extends": ["@mikey-pro/eslint-config-react"]
+  }
+}
+```
---
+++ eslint-config-react/index.js
@@ -0,0 +1,63 @@
+const path = require('node:path');
+
+module.exports = {
+  extends: ['@mikey-pro/eslint-config', 'react-app-bump'],
+  parserOptions: {
+    babelOptions: {
+      presets: [
+        [
+          '@babel/preset-react',
+          {
+            runtime: 'automatic',
+          },
+        ],
+      ],
+    },
+  },
+  rules: {
+    'react/display-name': [1, { ignoreTranspilerName: false }],
+    'react/function-component-definition': [
+      0,
+      { namedComponents: 'arrow-function' },
+    ],
+    'react/jsx-curly-spacing': 1,
+    'react/jsx-key': [1, { checkFragmentShorthand: true }],
+    'react/jsx-no-bind': [
+      1,
+      {
+        allowArrowFunctions: true,
+        allowFunctions: true,
+        ignoreRefs: true,
+      },
+    ],
+    'react/jsx-no-comment-textnodes': 1,
+    'react/jsx-no-duplicate-props': 1,
+    'react/jsx-no-target-blank': 1,
+    'react/jsx-no-undef': 1,
+    'react/jsx-tag-spacing': [1, { beforeSelfClosing: 'always' }],
+    'react/jsx-uses-vars': 1,
+    'react/no-danger': 1,
+    'react/no-deprecated': 1,
+    'react/no-did-mount-set-state': 1,
+    'react/no-did-update-set-state': 1,
+    'react/no-find-dom-node': 1,
+    'react/no-is-mounted': 1,
+    'react/no-string-refs': 1,
+    'react/prefer-es6-class': 1,
+    'react/prefer-stateless-function': 1,
+    'react/require-render-return': 1,
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: false,
+      },
+    ],
+    'react/state-in-constructor': 0,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
