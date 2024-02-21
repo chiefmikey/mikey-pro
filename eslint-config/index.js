46ea8a056d596fe9ce3beddb65df85f328f6068f
Mikl Wolfe <wolfemikl@gmail.com>
2024-02-20 21:21:50 -0700

    feat(config): init eslint react

--- eslint-config/index.js
+++ eslint-config/index.js
@@ -10,7 +10,6 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'react-app-bump',
     'plugin:unicorn/all',
     'plugin:compat/recommended',
     'plugin:css-modules/recommended',
@@ -173,12 +172,6 @@ module.exports = {
             },
           },
         ],
-        [
-          '@babel/preset-react',
-          {
-            runtime: 'automatic',
-          },
-        ],
       ],
     },
     ecmaVersion: 'latest',
@@ -308,46 +301,7 @@ module.exports = {
       },
     ],
     radix: 1,
-    'react/display-name': [1, { ignoreTranspilerName: false }],
-    'react/function-component-definition': [
-      0,
-      { namedComponents: 'arrow-function' },
-    ],
-    'react/jsx-curly-spacing': 1,
-    'react/jsx-key': [1, { checkFragmentShorthand: true }],
-    'react/jsx-no-bind': [
-      1,
-      {
-        allowArrowFunctions: true,
-        allowFunctions: true,
-        ignoreRefs: true,
-      },
-    ],
-    'react/jsx-no-comment-textnodes': 1,
-    'react/jsx-no-duplicate-props': 1,
-    'react/jsx-no-target-blank': 1,
-    'react/jsx-no-undef': 1,
-    'react/jsx-tag-spacing': [1, { beforeSelfClosing: 'always' }],
-    'react/jsx-uses-vars': 1,
-    'react/no-danger': 1,
-    'react/no-deprecated': 1,
-    'react/no-did-mount-set-state': 1,
-    'react/no-did-update-set-state': 1,
-    'react/no-find-dom-node': 1,
-    'react/no-is-mounted': 1,
-    'react/no-string-refs': 1,
     'filenames/match-regex': 0,
-    'react/prefer-es6-class': 1,
-    'react/prefer-stateless-function': 1,
-    'react/require-render-return': 1,
-    'react/self-closing-comp': [
-      'error',
-      {
-        component: true,
-        html: false,
-      },
-    ],
-    'react/state-in-constructor': 0,
     'require-atomic-updates': 0,
     'sort-imports': 0,
     'rest-spread-spacing': 0,
@@ -390,9 +344,6 @@ module.exports = {
   },
   settings: {
     polyfills: ['Promise'],
-    react: {
-      version: 'detect',
-    },
     jest: {
       version: 29,
     },
