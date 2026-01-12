ac1128e8905a06f3a8fcc81fa541997f68781189
Mikl Wolfe <wolfemikl@gmail.com>
2026-01-12 16:32:03 -0700

    refactor(theme): use published mikey-pro packages and add eslint.config.js

---
+++ theme/eslint.config.js
@@ -0,0 +1,6 @@
+// Modern ESLint 9 configuration for Mikey Pro Theme
+// Uses the @mikey-pro/eslint-config package
+
+import mikeyProConfig from '@mikey-pro/eslint-config';
+
+export default mikeyProConfig;
