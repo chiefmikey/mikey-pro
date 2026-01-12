af8e98048dc81487f69f8253e8b011d54058167b
Mikl Wolfe <wolfemikl@gmail.com>
2026-01-12 16:32:01 -0700

    refactor(config): use published mikey-pro packages instead of local file references

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 // Modern ESLint 9 configuration for Mikey Pro
 // Ultimate coding style guide for excellence
-// Uses the mikey-pro eslint-config package
+// Uses the @mikey-pro/eslint-config package
 
-import mikeyProConfig from 'mikey-pro';
+import mikeyProConfig from '@mikey-pro/eslint-config';
 
 export default mikeyProConfig;
